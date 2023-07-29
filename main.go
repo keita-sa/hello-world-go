f5416d93f451a1924721c9243da186c5deed68ef
Keita Sakurai <keitasakuraims@gmail.com>
2023-07-29 10:58:28 +0900

    added commentout

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path) // r.URL.Pathはリクエストのパスを返す, %sで文字列の表示
 	})
 
 	http.ListenAndServe(":80", nil)
